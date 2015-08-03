7f575850a3157c36ef07cd9c33cc17fdaa57e8b7
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-03 04:37:21 -0300

    sync

--- autoCam.sh
+++ autoCam.sh
@@ -4,12 +4,14 @@ while true; do
 	DATE=`date +%Y%m%d`
 	HOUR=`date +%H`
 	if [[ $HOUR -ge 10 && $HOUR -le 21 ]]; then
+		echo 'starting pic'
 		sudo raspistill -q 100 -rot 270 -ex auto -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
-		wait
+		echo 'copying pic'
 		sudo cp /home/pi/corcovadoStream/outputs/$DATETIME.jpg /usr/share/nginx/www/last.jpg
 	else
+		echo 'starting pic'
 		sudo raspistill -q 100 -rot 270 -ex night -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
-		wait
+		echo 'copying pic'
 		sudo cp /home/pi/corcovadoStream/outputs/$DATETIME.jpg /usr/share/nginx/www/last.jpg
 	fi
 	#sleep 3600
