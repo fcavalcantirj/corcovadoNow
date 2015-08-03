6e482c9bf38828276727e6fa34b0fd6622ff4e82
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-03 02:48:22 -0300

    autoCam

---
+++ autoCam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true; do 
+	DATETIME=$(date +"%Y-%m-%d_%H%M")
+	DATE=`date +%Y%m%d`
+	HOUR=`date +%H`
+	if [ $HOUR -ge 10 && $HOUR -le 21]
+	then
+		raspistill -q 100 -rot 270 -ex auto -o $DATETIME.jpg
+	else
+		raspistill -q 100 -rot 270 -ex night -o $DATETIME.jpg
+	fi
+	sleep 3600
+done
