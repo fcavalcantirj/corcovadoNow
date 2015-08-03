4c09ff2e1f670690a7cb3bc7bfbe9ae6947fb7f8
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-03 03:55:47 -0300

    sync

--- autoCam.sh
+++ autoCam.sh
@@ -4,9 +4,11 @@ while true; do
 	DATE=`date +%Y%m%d`
 	HOUR=`date +%H`
 	if [[ $HOUR -ge 10 && $HOUR -le 21 ]]; then
-		sudo raspistill -q 100 -rot 270 -ex auto -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
+		#sudo raspistill -q 100 -rot 270 -ex auto -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
+		sudo raspistill -q 100 -rot 270 -ex auto -o /home/pi/corcovadoStream/outputs/last.jpg
 	else
-		sudo raspistill -q 100 -rot 270 -ex night -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
+		#sudo raspistill -q 100 -rot 270 -ex night -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
+		sudo raspistill -q 100 -rot 270 -ex night -o /home/pi/corcovadoStream/outputs/last.jpg
 	fi
 	sleep 3600
 done
