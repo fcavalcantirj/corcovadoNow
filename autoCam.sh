1d41de074680e85b6fc79f58e0acf337a16c4fae
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-03 02:53:15 -0300

    autoCam

--- autoCam.sh
+++ autoCam.sh
@@ -3,8 +3,7 @@ while true; do
 	DATETIME=$(date +"%Y-%m-%d_%H%M")
 	DATE=`date +%Y%m%d`
 	HOUR=`date +%H`
-	if [ $HOUR -ge 10 && $HOUR -le 21 ];
-	then
+	if [ $HOUR -ge 10 && $HOUR -le 21 ]; then
 		raspistill -q 100 -rot 270 -ex auto -o $DATETIME.jpg
 	else
 		raspistill -q 100 -rot 270 -ex night -o $DATETIME.jpg
