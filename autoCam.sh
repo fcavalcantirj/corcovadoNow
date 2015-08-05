b1f4bae8ffe1b6d631dbfce5dde4561a058d9514
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-05 10:08:13 -0300

    syncing

--- autoCam.sh
+++ autoCam.sh
@@ -7,13 +7,13 @@ while true; do
 		sudo raspistill -h 700 -w 930 -q 100 -rot 270 -ex auto -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
 		wait
 		#sudo cp /home/pi/corcovadoStream/outputs/$DATETIME.jpg /usr/share/nginx/www/last.jpg
-		scp -i /home/pi/fcavalcanti.pem /home/pi/corcovadoStream/outputs/$DATETIME.jpg ec2-user@52.26.112.248:/usr/local/nginx/html/imgs/last.jpg
+		scp -o StrictHostKeyChecking=no -i /home/pi/fcavalcanti.pem /home/pi/corcovadoStream/outputs/$DATETIME.jpg ec2-user@52.26.112.248:/usr/local/nginx/html/imgs/last.jpg
 		wait
 	else
 		sudo raspistill -h 700 -w 930 -q 100 -rot 270 -ex night -o /home/pi/corcovadoStream/outputs/$DATETIME.jpg
 		wait
 		#sudo cp /home/pi/corcovadoStream/outputs/$DATETIME.jpg /usr/share/nginx/www/last.jpg
-		scp -i /home/pi/fcavalcanti.pem /home/pi/corcovadoStream/outputs/$DATETIME.jpg ec2-user@52.26.112.248:/usr/local/nginx/html/imgs/last.jpg
+		scp -o StrictHostKeyChecking=no -i /home/pi/fcavalcanti.pem /home/pi/corcovadoStream/outputs/$DATETIME.jpg ec2-user@52.26.112.248:/usr/local/nginx/html/imgs/last.jpg
 		wait
 	fi
 	sleep 90
