663a306bd520f36f5040f0b3e1289e9a126eae70
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-07 18:24:56 -0300

    syncing

--- colorCheck.py
+++ colorCheck.py
@@ -15,9 +15,9 @@ access_token_secret=os.environ.get('access_token_secret')
 print pix[1264, 834]
 
 colors = pix[1264, 834]
-maxDiffG = 150
-maxDiffB = 150
-maxDiffR = 90
+maxDiffG = 145
+maxDiffB = 145
+maxDiffR = 80
 
 defaultColor = 255
 
@@ -25,19 +25,26 @@ diffR = 255 - colors[0]
 diffG = 255 - colors[1]
 diffB = 255 - colors[2]
 
-#de dia
+#de dia...cinza.
 #(108, 97, 93)
 #diffR=147
 #diffG=158
 #diffB=162
-#as 7 e pouco
+
+#as 7 e pouco...branco
 #diffR=62
 #diffG=120
 #diffB=122
 
+#orange...avermelhado
+#(170, 91, 96)
+#diffR=85
+#diffG=164
+#diffB=159
+
 sentences = ["Check me out...I'm not white!","Check me out...I have a different color!","hummm...I think I`m different today!!!", "I'm feeling different today!!!", "Hey...I might have a strange color now..."]
 
-if diffG >= maxDiffG or diffB >= maxDiffB:
+if diffG >= maxDiffG and diffB >= maxDiffB and diffR >= maxDiffR:
 	random.shuffle(sentences)
 	print 'diffR=' + str(diffR)
 	print 'diffG=' + str(diffG)
