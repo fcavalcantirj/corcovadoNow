463663eac619cc5f3c8c6124e8a8d69b384c54df
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-07 16:54:17 -0300

    syncing

--- colorCheck.py
+++ colorCheck.py
@@ -2,11 +2,20 @@
 from PIL import Image
 from twitter import *
 import random
+import os
 
 im = Image.open("../corcovadoStream/outputs/last.jpg")
 pix = im.load()
 
+consumer_key=os.environ.get('consumer_key')
+consumer_secret=os.environ.get('consumer_secret')
+access_token_key=os.environ.get('access_token')
+access_token_secret=os.environ.get('access_token_secret')
+
 print pix[1264, 834]
+
+t = Twitter(auth=OAuth(consumer_key, consumer_secret, access_token_key, access_token_secret))
+t.statuses.update(status='#myfirstweetfromraspberrypi')
 
 colors = pix[1264, 834]
 maxDiffG = 50
@@ -19,6 +28,12 @@ diffR = 255 - colors[0]
 diffG = 255 - colors[1]
 diffB = 255 - colors[2]
 
+#de dia
+#(108, 97, 93)
+#diffR=147
+#diffG=158
+#diffB=162
+
 sentences = ["Check me out...I'm not white!","Check me out...I have a different color!","hummm...I think I`m different today!!!", "I'm feeling different today!!!", "Hey...I might have a strange color now..."]
 
 if diffG >= maxDiffG or diffB >= maxDiffB:
@@ -26,6 +41,6 @@ if diffG >= maxDiffG or diffB >= maxDiffB:
 	print 'diffR=' + str(diffR)
 	print 'diffG=' + str(diffG)
 	print 'diffB=' + str(diffB)
-	t = Twitter(auth=OAuth('3406707393-MGD1EGJrXGeItx4HyQPEt0bm67Qi2ABYsYgid2t', '4MjtcETEvyFyqIKN1Twz9TOrPPwPFCnu0UhR0Gw9MeerW', 'CGqzQPkZ0kKH9mOGvixkhtny1', 'GBjCXaXMRD5c5L2rBzUYAMsiEV5ues3BIHe7rJ1SL4qCdBxX0B'))
+	t = Twitter(auth=OAuth(consumer_key, consumer_secret, access_token_key, access_token_secret))
 	t.statuses.update(status=str(sentences[0]))
 	print 'just tweeted'
