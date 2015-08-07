9eb40029451533aad43cc0ee550889ec10c5ead9
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2015-08-07 07:45:34 -0300

    syncing

--- colorCheck.py
+++ colorCheck.py
@@ -8,8 +8,8 @@ pix = im.load()
 #print pix[1264, 834]
 colors = pix[1264, 834]
 maxDiffG = 50
-maxDiffB = 85
-maxDiffR = 85
+maxDiffB = 90
+maxDiffR = 90
 
 defaultColor = 255
 
@@ -17,7 +17,7 @@ diffR = 255 - colors[0]
 diffG = 255 - colors[1]
 diffB = 255 - colors[2]
 
-sentences = ["Check me out...I'm not white!","Check me out...I have a different color!","hummm...I think I`m different today!!!"]
+sentences = ["Check me out...I'm not white!","Check me out...I have a different color!","hummm...I think I`m different today!!!", "I'm feeling different today!!!", "Hey...I might have a strange color now..."]
 
 if diffG >= maxDiffG or diffB >= maxDiffB:
 	random.shuffle(sentences)
